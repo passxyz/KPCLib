f906d36dfb8d535b3dfe7f0a9b20d9a23c29f511
Roger Ye <shugaoye@yahoo.com>
2022-05-28 12:54:38 +0800

    Updated SkiaSharp and Svg.Skia

--- KeePassLib/Serialization/KdbxFile.Read.Streamed.cs
+++ KeePassLib/Serialization/KdbxFile.Read.Streamed.cs
@@ -176,7 +176,78 @@ namespace KeePassLib.Serialization
 			if(m_ctxGroups.Count != 0) throw new FormatException();
 		}
 
-		private KdbContext ReadXmlElement(KdbContext ctx, XmlReader xr)
+
+        /// <summary>
+        /// System.Drawing.ColorTranslator.FromHtml is missing in .NET 6. Copy from the source code temporarily.
+		/// https://github.com/dotnet/runtime/issues/21980
+        /// </summary>
+        /// <include file='doc\ColorTranslator.uex' path='docs/doc[@for="ColorTranslator.FromHtml"]/*' />
+        /// <devdoc>
+        ///    Translates an Html color representation to
+        ///    a GDI+ <see cref='System.Drawing.Color'/>.
+        /// </devdoc>
+        public static Color FromHtml(string htmlColor)
+        {
+            Color c = Color.Empty;
+
+            // empty color
+            if ((htmlColor == null) || (htmlColor.Length == 0))
+                return c;
+
+            // #RRGGBB or #RGB
+            if ((htmlColor[0] == '#') &&
+                ((htmlColor.Length == 7) || (htmlColor.Length == 4)))
+            {
+
+                if (htmlColor.Length == 7)
+                {
+                    c = Color.FromArgb(Convert.ToInt32(htmlColor.Substring(1, 2), 16),
+                                       Convert.ToInt32(htmlColor.Substring(3, 2), 16),
+                                       Convert.ToInt32(htmlColor.Substring(5, 2), 16));
+                }
+                else
+                {
+                    string r = Char.ToString(htmlColor[1]);
+                    string g = Char.ToString(htmlColor[2]);
+                    string b = Char.ToString(htmlColor[3]);
+
+                    c = Color.FromArgb(Convert.ToInt32(r + r, 16),
+                                       Convert.ToInt32(g + g, 16),
+                                       Convert.ToInt32(b + b, 16));
+                }
+            }
+
+            // special case. Html requires LightGrey, but .NET uses LightGray
+            if (c.IsEmpty && String.Equals(htmlColor, "LightGrey", StringComparison.OrdinalIgnoreCase))
+            {
+                c = Color.LightGray;
+            }
+
+            // System color
+            //if (c.IsEmpty)
+            //{
+            //    if (htmlSysColorTable == null)
+            //    {
+            //        InitializeHtmlSysColorTable();
+            //    }
+
+            //    object o = htmlSysColorTable[htmlColor.ToLower(CultureInfo.InvariantCulture)];
+            //    if (o != null)
+            //    {
+            //        c = (Color)o;
+            //    }
+            //}
+
+            // resort to type converter which will handle named colors
+            //if (c.IsEmpty)
+            //{
+            //    c = (Color)TypeDescriptor.GetConverter(typeof(Color)).ConvertFromString(htmlColor);
+            //}
+
+            return c;
+        }
+
+        private KdbContext ReadXmlElement(KdbContext ctx, XmlReader xr)
 		{
 			Debug.Assert(xr.NodeType == XmlNodeType.Element);
 
@@ -238,7 +309,7 @@ namespace KeePassLib.Serialization
 					{
 						string strColor = ReadString(xr);
 						if(!string.IsNullOrEmpty(strColor))
-							m_pwDatabase.Color = ColorTranslator.FromHtml(strColor);
+							m_pwDatabase.Color = FromHtml(strColor);
 					}
 					else if(xr.Name == ElemDbKeyChanged)
 						m_pwDatabase.MasterKeyChanged = ReadTime(xr);
@@ -445,13 +516,13 @@ namespace KeePassLib.Serialization
 					{
 						string strColor = ReadString(xr);
 						if(!string.IsNullOrEmpty(strColor))
-							m_ctxEntry.ForegroundColor = ColorTranslator.FromHtml(strColor);
+							m_ctxEntry.ForegroundColor = FromHtml(strColor);
 					}
 					else if(xr.Name == ElemBgColor)
 					{
 						string strColor = ReadString(xr);
 						if(!string.IsNullOrEmpty(strColor))
-							m_ctxEntry.BackgroundColor = ColorTranslator.FromHtml(strColor);
+							m_ctxEntry.BackgroundColor = FromHtml(strColor);
 					}
 					else if(xr.Name == ElemOverrideUrl)
 						m_ctxEntry.OverrideUrl = ReadString(xr);
